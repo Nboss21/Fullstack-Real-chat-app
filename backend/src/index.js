b4eb5c8b67f8ab24867bcc2e8b41054dd656b433
Natnael Esayas <Nanoboss44@gmail.com>
2025-07-04 12:30:55 +0200

    error fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,13 @@ import cors  from 'cors'
 import cookieParser from 'cookie-parser';
 import { app,server } from './lib/socket.js';
 import path from "path"
+
+
 dotenv.config()
+
+
 const PORT =process.env.PORT;
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -18,15 +22,16 @@ app.use(cors({
   origin:"http://localhost:5173",
   credentials:true
 }))
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if(process.env.NODE_ENV=== "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-  })
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 server.listen(PORT,()=>{
     console.log(
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,7 +7,7 @@ import {io} from "socket.io-client"
 
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
  export const useAuthStore = create((set, get) => ({
    authUser: null,
    isSigningUp: false,
